6d7373ad56f8bce98109631cc91394a5ddc488bc
crycetruly <crycetruly@gmail.com>
2019-10-21 21:11:32 +0200

    ch(starter): setup an expo react native boilerplate

---
+++ App.js
@@ -0,0 +1,11 @@
+import React,{useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
+}
+
